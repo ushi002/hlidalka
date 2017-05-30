535f4f67e80353285caebee6307099e98be9c297
Ludek Uhlir <uhlir@ufa.cas.cz>
2017-05-30 16:33:21 +0200

    Add thiefdetect pin, else sleep with watchdog

--- main.c
+++ main.c
@@ -20,6 +20,8 @@
 
 #define PRTC_ANAL    ANSELC
 
+#define CAMTRIG         PORTCbits.RC5
+#define CAMPWREN        PORTCbits.RC4
 /* i.e. uint8_t <variable_name>; */
 
 /******************************************************************************/
@@ -28,6 +30,7 @@
 void main(void)
 {
     unsigned int dacval, i;
+    bool thiefdetected = 0;
     
     /* Configure the oscillator for the device */
     ConfigureOscillator();
@@ -36,27 +39,29 @@ void main(void)
     InitApp();
     
     //turn off watchdog:
-    WDTCONbits.SWDTEN = 0; //check WDTE in config word1, must be 01 to enable 
+    WDTCONbits.SWDTEN = 1; //check WDTE in config word1, must be 01 to enable
                             //control
-    //WDTCONbits.WDTPS = 0xf; //default 2s
+    WDTCONbits.WDTPS = 0xc; //0xC=4s
     
     //configure SPI pins:
     //0 = output, 1 = input    
     TRISAbits.TRISA4 = 0;
     TRISAbits.TRISA5 = 1;
+
     TRISCbits.TRISC4 = 0;
     TRISCbits.TRISC5 = 0;
     //enable weak pull ups
-    OPTION_REGbits.nWPUEN = 0;
+    //OPTION_REGbits.nWPUEN = 0;
     //weak pull up
-    WPUAbits.WPUA5 = 1;
+    //WPUAbits.WPUA5 = 1;
     
     //set output value
     PORTAbits.RA4 = 0;
+    //PORTAbits.RA5 = 0;
     
     //set output value
-    PORTCbits.RC4 = 0;
-    PORTCbits.RC5 = 0;
+    CAMPWREN = 0;
+    CAMTRIG = 0;
     
     //enable DAC port output pin:
     DACCON0bits.DACOE1 = 1;
@@ -64,33 +69,76 @@ void main(void)
     DACCON0bits.D1PSS = 0;
     //enable DAC
     DACCON0bits.DACEN = 1;
-
+    ANSELAbits.ANSA4 = 0; //enable digital input for PORTA4
     dacval = 0;
     while(1)
     {
-        asm("CLRWDT");
-        for(i=0; i<20000; i++) //higher then 20000 values does not work - watchdog!
-        {            
+        if (!thiefdetected)
+        {
+            //enable interrupts
+            //GIE is for interrupt vectors. Else program continues just after the sleep
+            //INTCONbits.GIE = 1;
+            //enable external interrupt
+            INTCONbits.INTE = 1;
+            //enable interrupt on change
+            INTCONbits.IOCIE = 1;
+
+            //detect rising (P) falling (N) edge
+            IOCAPbits.IOCAP5 = 1;
+            //!! IOCAFbits.IOCAF5 should be set when interrupt arrives..
+            //??OPTION_REGbits.INTEDG = 0; //falling edge interrupt
+            //OPTION_REGbits
+            INTCONbits.INTF = 0; //external interrupt flag
+            while(1)
+            {
+                for(i=0; i<200; i++) {} //delay before diode is turned off
+                LATAbits.LATA4 = 0;
+                asm("SLEEP");
+                asm("NOP");
+                if (!STATUSbits.nTO) //watchdog timeout
+                    LATAbits.LATA4 = 1;
+                if (PCONbits.nRWDT)
+                    LATAbits.LATA4 = 1; //watchdog reset has not occurred
+                if(INTCONbits.IOCIF)
+                    LATAbits.LATA4 = 1; //some external interrupt flag occurred
+                INTCONbits.INTF = 0; //clear (watchdog) flag
+                if(IOCAFbits.IOCAF5)    //PORTA.5 flag
+                {
+                    //IOCAFbits.IOCAF5 = 0; //moved to the very end of the cycle
+                    thiefdetected = true;
+                    break; //quit this loop and continue the program
+                }
+            }
         }
-        PORTAbits.RA4 = LATAbits.LATA4 ^ 1;
+        asm("CLRWDT");
+        for(i=0; i<20000; i++) //higher then 20000 values does not work - watchdog! (OLD WATCHDOG)
+        {
+        }
+        LATAbits.LATA4 = LATAbits.LATA4 ^ 1; //heart beat
+
+        APFCONbits.P2SEL = 0;
 
         dacval++;
         DACCON1bits.DACR = dacval;  //5 bits copied
         dacval = DACCON1bits.DACR;
         
-        PORTCbits.RC4 = 1; //keep DC/DC on
-        PORTCbits.RC5 = 0; //shutter off
+        CAMPWREN = 1; //keep DC/DC on
+        CAMTRIG = 0; //shutter off
         
         if (dacval == 3 || dacval == 5) //shutter trigger
         {
-            PORTCbits.RC5 = 1;            
+            CAMTRIG = 1;
         }
         
+        //if (dacval > 3)  //speed up debugging
         if (dacval > 23)  //skip last 8 steps (31 steps)
         {
             dacval = 0;
-            PORTCbits.RC4 = 0;            
-        }        
+            DACCON1bits.DACR = 0;
+            CAMPWREN = 0;
+            thiefdetected = 0; //end of camera sequence
+            IOCAFbits.IOCAF5 = 0;
+        }
     }
     
     //enable interrupts
--- system.c
+++ system.c
@@ -17,6 +17,28 @@
 oscillator configurations. */
 void ConfigureOscillator(void)
 {
+    // CONFIG1
+#pragma config FOSC = INTOSC    // Oscillator Selection Bits (INTOSC oscillator: I/O function on CLKIN pin)
+//#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
+#pragma config WDTE = 1       // Watchdog Timer controlled by WDTCON register
+//#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
+//#pragma config MCLRE = OFF      // MCLR Pin Function Select (MCLR/VPP pin function is Digital Input)
+//#pragma config CP = OFF         // Flash Program Memory Code Protection (Program memory code protection is disabled)
+//#pragma config BOREN = ON       // Brown-out Reset Enable (Brown-out Reset enabled)
+//#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+//#pragma config IESO = OFF       // Internal/External Switchover Mode (Internal/External Switchover Mode is enabled)
+//#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is enabled)
+
+// CONFIG2
+//#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
+//#pragma config CPUDIV = NOCLKDIV // CPU System Clock Selection Bit (CPU system clock divided by 6)
+//#pragma config USBLSCLK = 48MHz // USB Low SPeed Clock Selection bit (System clock expects 48 MHz, FS/LS USB CLKENs divide-by is set to 8.)
+//#pragma config PLLMULT = 4x     // PLL Multipler Selection Bit (3x Output Frequency Selected)
+//#pragma config PLLEN = ENABLED  // PLL Enable Bit (3x or 4x PLL Enabled)
+//#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+//#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+//#pragma config LPBOR = OFF      // Low-Power Brown Out Reset (Low-Power BOR is disabled)
+//#pragma config LVP = OFF         // Low-Voltage Programming Enable (Low-voltage programming enabled)
     /* TODO Add clock switching code if appropriate.  */
     
     //APPLIED VALUE: default/reset value of OSCCON.IRCF is '0111' -> 500kHz
